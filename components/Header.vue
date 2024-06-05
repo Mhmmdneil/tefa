dc273e4f62be5f0871d4ebc0551bd95fc84dd27f
Mhmmdneil <141992443+Mhmmdneil@users.noreply.github.com>
2024-06-05 09:44:31 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="conten">
+    <div class="row p-3 text-black">
+      <div class="col-lg-1">
+        <img src="~/assets/img/LOGO-SMK4 2.png" alt="logo smkn 4">
+      </div>
+
+      <div class="col-lg-11">
+        <h1>Perpus Digital</h1>
+        <address>
+          <p>SMKN 4 TASIKMALAYA</p>
+          Jl,Depok, Sukamenak, Purbaratu
+        </address>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.conten{
+  background-color: rgb(5, 71, 73);
+  font-size: 25px;
+}
+
+.alamat{
+  font-family: jaldi;
+  font-size: 25px;
+}
+
+img {
+  width: 100px;
+}
+/*
+@media (max-width: 912px){
+  img {
+    width: 90px;
+}
+.judul {
+  letter-spacing: 10px;
+  font-size: 50px;
+  font-family: "jockey one";
+  padding-left: 20px;
+}
+.alamat {
+  font-family: "jaldi";
+  padding-left: 20px;
+  }
+}
+@media (max-width: 600px){
+  img{
+    width: 70px;
+}
+.alamat {
+  font-family: "jaldi";
+  padding-left: 20px;
+  }
+} */
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="conten">
+    <div class="container-fluid">
+      <div class="row py-5">
+        <div class="col-lg-6">
+          <nuxt-link to="/pengunjung">
+            <div class="card bg-pengunjung rounded-5">
+              <div class="card-body">
+                <h2>Pengunjung</h2>
+              </div>
+            </div>
+          </nuxt-link>
+      </div>
+      </div>
+  
+      <div class="col-lg-6">
+        <nuxt-link to="/buku">
+          <div class="card bg-buku rounded-5">
+            <div class="card-body">
+              <h2>Cari Buku</h2>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.conten{
+  background-color: #1291D8;
+}
+  .card {
+    height: 250px;
+    box-shadow: whitesmoke;
+  }
+  .card.bg-pengunjung {
+    background-image: url('../assets/img/cari.png');
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+  }
+  .card.bg-buku {
+    background: url('../assets/img/kunjungan.png') no-repeat center center;
+    background-size: cover;
+  }
+</style>
