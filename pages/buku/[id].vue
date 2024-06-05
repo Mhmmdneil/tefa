37684a831bdc8ef6adfb5cd5766e973d2fa85a46
Mhmmdneil <141992443+Mhmmdneil@users.noreply.github.com>
2024-06-05 14:53:10 +0700

    Add files via upload

--- pages/buku/[id].vue
+++ pages/buku/[id].vue
@@ -5,18 +5,18 @@
       <img :src="buku?.cover" class="cover" alt="cover buku" style="width: 300px;">
     </div>
     <div class="col-lg-8 col-12">
-                    <div class="row">
-                        <h1>{{ buku?.judul }}</h1>
-                    </div>
-                    <div class="row">
-                        <h4>Pengarang : {{ buku?.penulis }}</h4>
-                        <h4>Penerbit : {{ buku?.penerbit }}</h4>
-                        <h4>Tahun Terbit : {{ buku?.tahun_terbit }}</h4>
-                        <h4>Genre : {{ buku?.genre }}</h4>
-                    </div>
-                    <div class="row">
-                        <h6>{{ buku?.deskripsi }}</h6>
-                    </div>
+      <div class="row">
+        <h1>{{ buku?.judul }}</h1>
+      </div>
+      <div class="row">
+        <h4>Pengarang : {{ buku?.penulis }}</h4>
+        <h4>Penerbit : {{ buku?.penerbit }}</h4>
+        <h4>Tahun Terbit : {{ buku?.tahun_terbit }}</h4>
+        <h4>Genre : {{ buku?.genre }}</h4>
+      </div>
+      <div class="row">
+        <h6>{{ buku?.deskripsi }}</h6>
+      </div>
     </div>
   </div>
 </template>
@@ -31,50 +31,42 @@ const buku = ref([])
 
 
 const getBookById = async () => {
-    const { data, error } = await supabase.from('buku')
+  const { data, error } = await supabase.from('buku')
     .select(`*, kategori_buku(*)`)
     .eq('id', route.params.id)
     .maybeSingle()
-    if (!error) buku.value=data
+  if (!error) buku.value = data
 }
 
 onMounted(() => {
-    getBookById()
+  getBookById()
 })
 </script>
 
 <style scoped>
-.wrapper{ 
-    color: #fffeee;
-    letter-spacing: 1.2px;
+.wrapper {
+  color: #fffeee;
+  letter-spacing: 1.2px;
 }
 
-.content {
-    background-image: url('@/assets/img/bg-rincianbuku.jpg');
-    background-size: cover;
-    background-repeat: no-repeat;
-    position: fixed;
-    top: 0;
-    background-position: center;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    z-index: -1;
-}
+
 .btn {
-    background-color: #fffeee;
-    font-family: 'Jockey One';
+  background-color: #fffeee;
+  font-family: 'Jockey One';
 }
+
 h1 {
-    font-family: "Jockey One";
+  font-family: "Jockey One";
 }
-h4{
-    font-family: "Jaldi"; 
-    font-size: 30px;
+
+h4 {
+  font-family: "Jaldi";
+  font-size: 30px;
 }
+
 h6 {
-    font-family: "Jaldi";
-    font-size: 20px;
-    letter-spacing: 2px;
+  font-family: "Jaldi";
+  font-size: 20px;
+  letter-spacing: 2px;
 }
 </style>
